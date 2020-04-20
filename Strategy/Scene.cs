a787c0909c104b40a4e675df6f70afc4660c50ed
Fr1tzler <Char1ieBrav0>
2020-04-21 01:15:50 +0500

    Version 0.08

--- Strategy/Scene.cs
+++ Strategy/Scene.cs
@@ -8,10 +8,10 @@ namespace strategy
 {
     public class Scene
     {
-        private const float TriangleSize = 40;
-        private const int MaxHeight = 25;
-        private const int SizeX = 20;
-        private const int SizeY = 20;
+        private const float PolygonSize = 20;
+        private const int MaxHeight = 2;
+        private const int SizeX = 40;
+        private const int SizeY = 40;
         private const int ShiftX = 960;
         private const int ShiftY = 100;
         private const float MapMovingVelocity = 10;
@@ -22,16 +22,18 @@ namespace strategy
         private ConvexShape _activePolygon;
         private List<ConvexShape> _warFog;
         private List<Unit> _units;
+        private const int HeightCoefficient = 5;
+        private static Color warFogColor;
+        private static Color orange;
+        
         
         public Scene(int seed)
         {
+            warFogColor = new Color(20, 20, 20, 160);
+            orange = new Color(178, 70, 0);
             _units = new List<Unit>();
-
-            /////
-
             _units.Add(new Unit(100, 1, 1, 1, 1, new Vector2f(500, 500), 1, 200));
             _units.Add(new Unit(100, 1, 1, 1, 1, new Vector2f(250, 900), 1, 350));
-            /////          
             _depthMap = new byte[SizeX + 1, SizeY + 1];
             _pointOfView = new Vector2f(0, 25);
             _cursorPosition = new Vector2f(Game.ScreenWidth / 2f, Game.ScreenHeight / 2f);
@@ -48,49 +50,33 @@ namespace strategy
             _map = new List<ConvexShape>();
             for (var x = 0; x < SizeX; x++)
                 for (var y = 0; y < SizeY; y++)
-                {
-                    _map.Add(GetPolygon(new Vector2i(x, y), new Vector2i(0, 0), true));
-                    _map.Add(GetPolygon(new Vector2i(x, y), new Vector2i(1, 1), true));
-                }
+                    _map.Add(GetPolygon(new Vector2i(x, y), true));
         }
 
         private void GenerateWarFog()
         {
             _warFog = new List<ConvexShape>();
             for (var x = 0; x < SizeX; x++)
-            for (var y = 0; y < SizeY; y++)
-            {
-                _warFog.Add(GetPolygon(new Vector2i(x, y), new Vector2i(0, 0), false));
-                _warFog.Add(GetPolygon(new Vector2i(x, y), new Vector2i(1, 1), false));
-            }
+                for (var y = 0; y < SizeY; y++)
+                    _warFog.Add(GetPolygon(new Vector2i(x, y), false)); 
         }
         
-        private static ConvexShape GetPolygon(Vector2i self, Vector2i shift, bool fuck)
+        private static ConvexShape GetPolygon(Vector2i self, bool notWarFog)
         {
-            var polygon = new ConvexShape(3);
-            var temp = self + shift;
-            polygon.SetPoint(0, GetMapPoint(temp.X, temp.Y, _depthMap[temp.X, temp.Y]));
+            var polygon = new ConvexShape(4);
+            polygon.SetPoint(0, GetMapPoint(self.X, self.Y, _depthMap[self.X, self.Y]));
             polygon.SetPoint(1, GetMapPoint(self.X + 1, self.Y, _depthMap[self.X + 1, self.Y]));
-            polygon.SetPoint(2, GetMapPoint(self.X, self.Y + 1, _depthMap[self.X, self.Y + 1]));
-            var averageHeight = (_depthMap[temp.X, temp.Y] + _depthMap[self.X + 1, self.Y] + _depthMap[self.X, self.Y + 1]) / 3;
-            var redComponent = (byte)(averageHeight * 5 + 128);
-            var greenComponent = (byte)(averageHeight * 2 + 50);
-            if (fuck)
-            {
-                polygon.FillColor = new Color(redComponent, greenComponent, 0);
-            }
-            else
-            {
-                polygon.FillColor = new Color(20, 20, 20, 160);
-            }
+            polygon.SetPoint(2, GetMapPoint(self.X + 1, self.Y + 1, _depthMap[self.X + 1, self.Y + 1]));
+            polygon.SetPoint(3, GetMapPoint(self.X, self.Y + 1, _depthMap[self.X, self.Y + 1]));
+            polygon.FillColor = notWarFog ? orange : warFogColor;
             polygon.OutlineColor = Color.Black;
             polygon.OutlineThickness = 1;
             return polygon;
         }
 
-        private static Vector2f GetMapPoint(int x, int y, byte depth)
+        private static Vector2f GetMapPoint(float x, float y, byte depth)
         {
-            return new Vector2f(2 * (-x + y) * TriangleSize + ShiftX, (x + y) * TriangleSize + ShiftY + depth);
+            return new Vector2f(2 * (-x + y) * PolygonSize + ShiftX, (x + y) * PolygonSize + ShiftY + depth * HeightCoefficient);
         }
 
         public void Update()
@@ -109,12 +95,8 @@ namespace strategy
             for (var x = 0; x < SizeX; x++) 
                 for (var y = 0; y < SizeY; y++) 
                     foreach (var unit in _units)
-                        if (MathModule.Length(GetMapPoint(x, y, _depthMap[x, y]) - unit.sprite.Position + _pointOfView) < unit.ViewRadius) 
-                        {
-                            _warFog[SizeY * x * 2 + y * 2].FillColor = Color.Transparent; 
-                            _warFog[SizeY * x * 2 + y * 2 + 1].FillColor = Color.Transparent;
-                        }
-            
+                        if (MathModule.Length(GetMapPoint(x + 0.5f, y + 0.5f, _depthMap[x, y]) - unit.sprite.Position + _pointOfView) < unit.ViewRadius)
+                            _warFog[SizeY * x + y].FillColor = Color.Transparent;
             _cursorPosition = (Vector2f) Mouse.GetPosition();
             var movement = Controls.GetArrowsState() * -MapMovingVelocity;
             _pointOfView += movement;
